9d1ead3aa8e801f3826921141f2bb5c33388619a
donishyar <ahmadshahdonishyar@gmail.com>
2023-12-19 21:44:05 +0430

    auth added

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AskQuestion = () => {
+  return (
+    <main className='flex min-h-screen w-full items-center justify-center'>Ask Question</main>
+  )
+}
+
+export default AskQuestion
