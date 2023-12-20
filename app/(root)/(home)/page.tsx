dc2dca49cc7e3941652bfa823b0eb0c7efa320fb
donishyar <ahmadshahdonishyar@gmail.com>
2023-12-20 20:39:12 +0430

    implement theme provider

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,9 @@
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-    return <UserButton afterSignOutUrl="/" />
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,54 +1,54 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import './globals.css'
-import React from 'react'
-import { Inter , Space_Grotesk } from 'next/font/google'
-import type { Metadata } from 'next'
- 
-const inter = Inter({ 
-  subsets: ['latin'],
-  weight:["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-inter", 
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/context/ThemeProvider";
 
-})
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
 
-const spaceGrotesk = Space_Grotesk({ 
-  subsets: ['latin'],
-  weight:["300", "400", "500", "600", "700"],
-  variable: "--font-spaceGrotesk", 
-
-})
-
-
-
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
 export const metadata: Metadata = {
-  title: 'DevFlow',
-  description: 'A community-driven platform for developers to learn, share, and connect.',
+  title: "DevFlow",
+  description:
+    "A community-driven platform for developers to learn, share, and connect.",
   icons: {
-    icon: '/assets/images/site-logo.svg',
-  }
-}
+    icon: "/assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-      appearance={{
-        elements: {
-          formButtonPrimary: 'primary-gradient',
-          footerActionLink: 
-          'primary-text-gradient hover:text-primary-500'
-        }
-      }}
-    
-    >
-      <html lang="en">
-        <h1 className='text-3xl'>Hello World</h1>
-        <body className={`${inter.variable} ${spaceGrotesk.variable} font-sans`}>{children}</body>
-      </html>
-    </ClerkProvider>
-  )
+    <html lang="en">
+      <h1 className="text-3xl">Hello World</h1>
+      <body
+        className={`${inter.variable} 
+        ${spaceGrotesk.variable} font-sans`}
+      >
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
 }
